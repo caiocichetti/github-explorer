dc799888dcede0b4717333371c2b271588ab64bd
Caio Cichetti <caio_cichetti@outlook.com>
2021-03-21 14:04:11 -0300

    feat: navigation between routes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
-import {} from 'react-router-dom';
 import api from '../../services/api';
 import { Title, Form, Error, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
@@ -78,7 +78,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="">
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -88,7 +91,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
